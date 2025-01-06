a9be495def3a9e598a79f27c7621cc81469175e5
SANJEET2002 <sanjeetistc138@gmail.com>
2025-01-06 13:11:25 +0530

    feat: initial setup done with webpack and tailwindcss and typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen w-screen overflow-auto">
+      this is main Container
+    </div>
+  );
+};
+
+export default App;
